f08749066c0a77b247feeabb48648705e8c877f7
AbduQahhor <abduqahhor.muhamadjonov@gmail.com>
2023-12-12 23:39:36 +0500

    update indentation

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,50 +1,50 @@
 class NotesController < ApplicationController
-	before_action :set_note, only: [:show, :edit, :update, :destroy]
+  before_action :set_note, only: [:show, :edit, :update, :destroy]
 
-	def index
-		@notes = Note.all
-	end
+  def index
+    @notes = Note.all
+  end
 
-	def show
-	end
+  def show
+  end
 
-	def new
-		@note = Note.new
-	end
+  def new
+    @note = Note.new
+  end
 
-	def create
-		@note = Note.new(note_params)
+  def create
+    @note = Note.new(note_params)
 
-		if @note.save
-			redirect_to notes_path, notice: "Note was successfully created!"
-		else
-			render :new
-		end
-	end
+    if @note.save
+      redirect_to notes_path, notice: "Note was successfully created!"
+    else
+      render :new
+    end
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def update
-		if @note.update(note_params)
-			redirect_to notes_path, notice: "Note was successfully updated!"
-		else
-			render :edit
-		end
-	end
+  def update
+    if @note.update(note_params)
+      redirect_to notes_path, notice: "Note was successfully updated!"
+    else
+      render :edit
+    end
+  end
 
-	def destroy
-		@note.destroy
-		redirect_to notes_path, notice: "Note was successfully removed!"
-	end
+  def destroy
+    @note.destroy
+    redirect_to notes_path, notice: "Note was successfully removed!"
+  end
 
-	private
+  private
 
-	def set_note
-		@note = Note.find(params[:id])
-	end
+  def set_note
+    @note = Note.find(params[:id])
+  end
 
-	def note_params
-		params.require(:note).permit(:title)
-	end
+  def note_params
+    params.require(:note).permit(:title)
+  end
 end
