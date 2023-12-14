060dfc5064f2746ffb9773f1c17f639e2cd40974
AbduQahhor <abduqahhor.muhamadjonov@gmail.com>
2023-12-14 12:15:42 +0500

    add real time udated with turbo streams and action cable

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,11 @@ class Note < ApplicationRecord
   validates :title, presence: true
 
   scope :desc, -> { order(created_at: :desc)}
+
+  # after_create_commit -> { broadcast_prepend_later_to "notes", partial: "notes/note", locals: { note: self }, target: "notes" }
+  # # after_create_commit -> { broadcast_prepend_to "quotes" } # this is shorthand of above - rails convention
+  # after_update_commit -> { broadcast_replace_later_to "notes" }
+  # after_destroy_commit -> { broadcast_remove_to "notes" }
+  # all above three is can be written with single line of conde like below - rails syntactic sugar
+  broadcasts_to ->(note) { "notes" }, inserts_by: :prepend
 end
