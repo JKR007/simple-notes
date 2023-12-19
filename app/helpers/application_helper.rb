a96549d2f94290dd02dff8708dffe1700fa6fa7e
AbduQahhor <abduqahhor.muhamadjonov@gmail.com>
2023-12-19 18:05:52 +0500

    refactor error messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def form_error_messages(object)
+    if object.errors.any?
+      tag.div class: "error-message" do
+        object.errors.full_messages.to_sentence.capitalize
+      end
+    end
+  end
 end
